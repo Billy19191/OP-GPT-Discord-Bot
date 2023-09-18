80947c9f46db9ad6ac264a507f3a9ffc5b78a15e
Billy19191 <ratchanon@Billy191-s-MacBook-Pro.local>
2023-09-19 01:09:46 +0700

    feat: merge tensorflow in node.js

--- index.ts
+++ index.ts
@@ -1,7 +1,9 @@
-import { Client, Collection, Events, GatewayIntentBits } from 'discord.js'
+import { Client, GatewayIntentBits } from 'discord.js'
 import * as tf from '@tensorflow/tfjs-node'
 import dotenv from 'dotenv'
+
 dotenv.config()
+
 const client = new Client({
   intents: [
     GatewayIntentBits.Guilds,
@@ -10,17 +12,56 @@ const client = new Client({
   ],
 })
 
-// const model = await tf.loadLayersModel('path/to/your/model.h5')
-const askModel = (message: string) => {}
+async function loadModel() {
+  // Load the model architecture (JSON)
+  const modelArchitecture = 'http://127.0.0.1:8080/model/config.json'
+
+  // Load the model
+  const model = await tf.loadGraphModel(modelArchitecture)
+
+  return model
+}
+
+async function makePrediction(input: string) {
+  try {
+    // Load the model
+    const model = await loadModel()
+
+    // Prepare the input data as a tensor (you may need preprocessing)
+    const inputTensor = tf.tensor([input])
+
+    // Make a prediction using the loaded model
+    const predictions = model.predict(inputTensor)
+
+    // Extract values from the prediction tensors and convert to JavaScript arrays
+    const predictionValues = await Promise.all(
+      (Array.isArray(predictions) ? predictions : [predictions]).map(
+        async (tensor) => {
+          // Cast the tensor to the tf.Tensor type to ensure that it has the data() method
+          const tensorAsTfTensor = tensor as tf.Tensor
+
+          const data = await tensorAsTfTensor.data()
+          return Array.from(data)
+        }
+      )
+    )
+
+    return predictionValues
+  } catch (error) {
+    console.error('Error making prediction:', error)
+    return 'An error occurred during prediction.'
+  }
+}
 
 client.on('messageCreate', async (message) => {
   if (message.author.bot) return
   if (message.content) {
     console.log('Message' + message.content)
-    // const prediction = await askModel(message.content)
-    message.reply('Test Integration // Waiting for model *TFJS-Node')
+    const prediction = await makePrediction(message.content)
+    message.reply(`Prediction: ${JSON.stringify(prediction)}`)
   }
 })
+
 client
   .login(process.env.DISCORD_TOKEN)
   .then(() => console.log('Bot is ready'))
